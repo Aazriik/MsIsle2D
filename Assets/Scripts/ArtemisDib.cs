f8c6d01054cafccfab54e560e94911839b3b3e9a
Aazriik <dibartemis@gmail.com>
2026-06-03 12:44:07 -0400

    Starting MainMenu UI with UI Toolkit

---
+++ Assets/Scripts/ArtemisDib.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ArtemisDib : MonoBehaviour
+{
+    void BusinessCard()
+    {
+        string name     = "ArtemisDib";
+        string title    = "Programmer";
+        string email    = "dibartemis@gmail.com";
+        string url      = "github.com/Aazriik";
+    }
+    
+
+    
+    
+    void Programmer()
+    {
+
+    }
+
+
+
+        
+
+
+}
